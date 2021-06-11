3be8328f001843aec622ff55289d5ccc67d27fdd
Ted Suzman <ted@suzman.net>
2021-06-11 13:54:12 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <div class="polis" data-conversation_id="7rspmntfxw" data-ucw="false" />
+    </div>
+  );
+}
+
+function sign(message, privateKey) {
+  // stuff happens
+  const signature = 1; // .. a bucnch focppuation
+  return signature;
+}
+
+function verify(message, signature, publicKey) {
+  // return true or false
+}
