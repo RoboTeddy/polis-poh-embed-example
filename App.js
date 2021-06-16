31565b62dfd9efa715b337b536a90f21426ad033
Colin Megill <colinmegill@gmail.com>
2021-06-15 19:13:41 -0700

    lock file

--- App.js
+++ App.js
@@ -3,10 +3,24 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <div class="polis" data-conversation_id="7rspmntfxw" data-ucw="false" />
+      <h1> Welcome to dao governance land </h1>
+      <Convo shouldshow={foo ? false : true} />
+      <div
+        class="polis"
+        data-conversation_id="7rspmntfxw"
+        xid={`poh___${metamaskPublicKey}___${signature}`}
+        data-ucw="false"
+      />
     </div>
   );
 }
+
+/* 
+
+  Do you need to register to even show the conversation? 
+  When does Polis create a user record / set a cookie? Does it for everyoen visitign closed convos on scoop?
+
+*/
 
 function sign(message, privateKey) {
   // stuff happens
