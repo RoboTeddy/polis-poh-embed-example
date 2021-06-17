ccbb78fdefb001875e1f868524b3fe06f4218d5f
Ted Suzman <ted@suzman.net>
2021-06-17 14:26:42 -0700

    check humanity contract for registration

--- App.js
+++ App.js
@@ -1,12 +1,116 @@
-import React from "react";
+import "regenerator-runtime/runtime";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+
+import humanityAbi from "./proofOfHumanityAbi";
+const proofOfHumanityAddress = "0xc5e9ddebb09cd64dfacab4011a0d5cedaf7c9bdb";
+
+const web3 = new Web3(
+  "wss://mainnet.infura.io/ws/v3/ec39a81039c945bd9063af6003109bf2"
+);
+
+const humanityContract = new web3.eth.Contract(
+  humanityAbi,
+  proofOfHumanityAddress
+);
+
+humanityContract.methods
+  .isRegistered("0x34cf1af38adf9855d02062d044e36f351a7f9120")
+  .call()
+  .then(function (result) {
+    console.log("isRegistered result", result);
+  })
+  .catch(function (err) {
+    console.log("isRegistered", err);
+  });
+
+// TODO: handle missing window.ethereum
+
+// TODO: figure out how to handle state where we've requested connection to metamask, but not actually connected, and then the user reloads
+
+// consider behavior on page reload
+
+// 1. get address
+
+// 2. find out if one of those addresses is registered with proof of humanity
+// --> if none are registered, let the user know that
+// --> otherwise, show liek "connected to poh" state of some sort
+
+// 3. request that you sign a pol.is login string --> that becomes the xid
+
+// (user uses application...)
+
+// 4. filter out invalid xids (server side)
+
+function getIsRegisteredInProofOfHumanity(address) {}
 
 export default function App() {
+  const [isConnected, setIsConnected] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  const metamaskPublicKey = "";
+  const signature = "";
+
+  function connect() {
+    ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then((result) => {
+        if (result.length) {
+          setAddress(result[0]);
+          setIsConnected(true);
+        } else {
+          alert("No addresses found");
+        }
+      })
+      .catch((err) => {
+        setIsConnected(false);
+        setAddress(null);
+        if (err.code === -32002) {
+          alert("Please accept the metamask connection");
+          console.log("caught error during connection", err);
+        }
+      });
+  }
+
+  useEffect(() => {
+    ethereum.on("connect", () => {
+      console.log("metamask connected");
+    });
+
+    ethereum.on("disconnect", () => {
+      console.log("metamask disconnected");
+    });
+
+    // TODO: figuer out if this event is supposed to trigger on page load
+    // if already connected (metamask docs seemed to say yes, but it doesn't happen)
+    ethereum.on("accountsChanged", (accounts) => {
+      console.log("accountsChanged", accounts);
+      setIsConnected(accounts.length > 0);
+      setAddress(accounts[0]);
+      // Handle the new accounts, or lack thereof.
+      // "accounts" will always be an array, but it can be empty.
+    });
+
+    ethereum.on("chainChanged", (chainId) => {
+      console.log("chainChanged", accounts);
+      alert("chain changed, this forces a reload"); // TODO: kill this alert
+      window.location.reload();
+    });
+
+    // TODO: return a function that would stop listening to these things
+  }, []);
+
   return (
     <div>
       <h1> Welcome to dao governance land </h1>
-      <Convo shouldshow={foo ? false : true} />
+
+      <div>
+        {isConnected ? "Connected " + address : "Not connected"}
+        {!isConnected && <button onClick={connect}>Connect</button>}
+      </div>
+
       <div
-        class="polis"
+        className="polis"
         data-conversation_id="7rspmntfxw"
         xid={`poh___${metamaskPublicKey}___${signature}`}
         data-ucw="false"
@@ -16,12 +120,6 @@ export default function App() {
 }
 
 /* 
-
-  Do you need to register to even show the conversation? 
-  When does Polis create a user record / set a cookie? Does it for everyoen visitign closed convos on scoop?
-
-*/
-
 function sign(message, privateKey) {
   // stuff happens
   const signature = 1; // .. a bucnch focppuation
@@ -31,3 +129,5 @@ function sign(message, privateKey) {
 function verify(message, signature, publicKey) {
   // return true or false
 }
+
+*/
